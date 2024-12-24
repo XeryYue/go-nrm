ebfe3b5db0540dc2f123858651eb6556fc1514cf
kanno <812137533@qq.com>
2024-12-25 07:41:58 +0800

    chore: stash

--- ini/parse.zig
+++ ini/parse.zig
@@ -3,43 +3,69 @@ const lex = @import("./lex.zig");
 
 const Token = lex.Token;
 const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
-pub const Node = struct {};
+pub const Node = struct {
+    kind: NodeKind,
+    pub const NodeKind = enum(u3) {
+        paris,
+        section,
+        comment,
+    };
+    pub const Paris = struct {
+        base: Node = .{ .kind = NodeKind.paris },
+    };
+    pub const Section = struct {
+        base: Node = .{ .kind = NodeKind.section },
+    };
+    pub const Comment = struct {
+        base: Node = .{ .kind = NodeKind.comment },
+    };
+};
 
-pub const AST = struct {};
+pub const AST = struct {
+    nodes: []Node,
+};
 
 pub const Parse = struct {
     allocator: Allocator,
+    ast: AST,
     tokens: []Token,
     const Self = @This();
     pub fn init(allocator: Allocator) Self {
         return Self{
             .allocator = allocator,
             .tokens = undefined,
+            .ast = undefined,
         };
     }
 
     pub fn parse(self: *Self, buffer: []const u8) !void {
         const result = try lex.Tokenizer(buffer, self.allocator);
+        const nodes = ArrayList(Node).init(self.allocator);
         defer {
             result.deinit();
+            nodes.deinit();
         }
         self.tokens = result.items;
-
+        // std.debug.print("{any}\n", .{self.tokens});
         for (self.tokens) |token| {
             switch (token.kind) {
                 Token.Kind.end_of_file => {
                     break;
                 },
                 Token.Kind.string => {
-                    //
+                    try nodes.append(self.consume_key_value_pairs());
                 },
+                // Token.Kind.open_bracket => {},
                 else => {},
             }
         }
     }
 
-    fn consume_key_value_pairs() void {}
+    fn consume_key_value_pairs(self: *Self) void {
+        _ = self.current();
+    }
 
     fn consume_comment() void {}
 
